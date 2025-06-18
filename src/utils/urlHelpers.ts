6df7f60ce5ce0f265833919440604f32ebce9c41
Shahin Mameghani <shahin.mameghani@meshconnect.com>
2025-06-18 22:13:28 +0200

    Fix returned type of urlSearchParams function

--- src/utils/urlHelpers.ts
+++ src/utils/urlHelpers.ts
@@ -3,6 +3,14 @@ import queryString from 'query-string';
 export const urlSearchParams = (url?: string) => {
   if (!url) return {};
 
-  const parsedUrl = queryString.parseUrl(url);
-  return parsedUrl.query;
+  const query = queryString.parseUrl(url).query;
+  const result: Record<string, string> = {};
+
+  for (const [key, value] of Object.entries(query)) {
+    if (typeof value === 'string') {
+      result[key] = value;
+    }
+  }
+
+  return result;
 };
