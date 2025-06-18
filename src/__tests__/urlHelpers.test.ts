f85b2efb3d41dce2915b4bee7040bc6c6044bea6
Shahin Mameghani <shahin.mameghani@meshconnect.com>
2025-06-18 23:42:27 +0200

    Refactor

--- src/__tests__/urlHelpers.test.ts
+++ src/__tests__/urlHelpers.test.ts
@@ -5,7 +5,7 @@ describe('urlSearchParams function', () => {
     const urls: Record<string, Record<string, string>> = {
       'https://example.com' : {},
       'https://example.com?foo=bar&baz=qux' : { baz : 'qux', foo: 'bar' },
-      'https://example.com?foo=bar#anchor' : { foo: 'bar' },
+      'https://example.com?foo=bar#anchor=ignored' : { foo: 'bar' },
       'https://example.com?search=query&sort' : { search: 'query' },
       'https://example.com?test%20encoded=value' : { 'test encoded': 'value' },
       'https://example.com/path?name=John&age=30' : { age: '30', name: 'John' },
