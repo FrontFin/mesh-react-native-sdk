4d2b5648e9653525291a600e327822df31213eeb
Shahin Mameghani <shahin.mameghani@meshconnect.com>
2025-06-18 22:06:43 +0200

    Replace regex with library function for parsing URL query params

--- src/utils/urlHelpers.ts
+++ src/utils/urlHelpers.ts
@@ -1,13 +1,8 @@
+import queryString from 'query-string';
+
 export const urlSearchParams = (url?: string) => {
-  if(!url) return {};
+  if (!url) return {};
 
-  const regex = /[?&]([^=#]+)=([^&#]*)/g;
-  const params: Record<string, string> = {};
-  let match;
-
-  while (match = regex.exec(url)) {
-    params[match[1]] = match[2];
-  }
-
-  return params;
+  const parsedUrl = queryString.parseUrl(url);
+  return parsedUrl.query;
 };
